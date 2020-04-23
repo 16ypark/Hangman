c6a127cd06d89a8e70ff92c152e7a7c64880b839
Yoo Hwa Park <16ypark@kaist.ac.kr>
2020-04-23 09:47:44 +0900

    init project

---
+++ Hangman/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Yoo Hwa Park on 2020/04/23.
+//  Copyright © 2020 Yoo Hwa Park. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
